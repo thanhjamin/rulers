b87ecf794d4e0c99b3df11855499fe1f1ab3c010
Thanh Huynh <thanhjamin@yahoo.com>
2013-11-09 16:11:10 -0800

    add routes

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -6,6 +6,23 @@ module Rulers
     include Rulers::Model
     def initialize(env)
       @env = env
+      @routing_params = {}
+    end
+
+    def dispatch(action, routing_params ={})
+      @routing_params = routing_params
+      text= self.send(action)
+      if get_response
+        st, hd, rs = get_response.to_a
+        [st, hd, [rs].flatten]
+      else
+        [200, {'Content-Type' => 'text.html'},
+          [text].flatten]
+      end
+    end
+
+    def self.action(act, rp = {})
+      proc { |e| self.new(e).dispatch(act, rp) }
     end
 
     def env
@@ -34,7 +51,7 @@ module Rulers
     end
 
     def params
-      request.params
+      request.params.merge @routing_params
     end
 
     def response(text, status = 200, headers = {})
